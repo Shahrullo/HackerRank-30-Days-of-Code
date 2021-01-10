7d82f80b22ae31e13866a8f545154eb7a0ae3f56
Shahrullo <shahrullo1995@mail.ru>
2021-01-10 09:57:14 +0900

    Day 2 First Commit

---
+++ Day 2/day2.py
@@ -0,0 +1,22 @@
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+def solve(meal_cost, tip_percent, tax_percent):
+
+    if __name__ == '__main__':
+        meal_cost = float(input())
+    
+        tip_percent = int(input())
+    
+        tax_percent = int(input())
+        
+        tip = (meal_cost / 100) * tip_percent
+        tax = (meal_cost / 100) * tax_percent
+        tot = meal_cost + tip + tax
+        return round(tot)
+    
+    
