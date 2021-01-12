8f5db994eafb5b95e8ebfdfd15eefda9bdbc5d0b
Shahrullo <shahrullo1995@mail.ru>
2021-01-13 07:22:30 +0900

    Day 6 First Commit

--- Day 0/day0.py
+++ Day 0/day0.py
@@ -1,18 +1,8 @@
-i = 4
-d = 4.0
-s = 'HackerRank '
-# Declare second integer, double, and String variables.
-i2 = None
-d2 = None
-s2 = None
-# Read and save an integer, double, and String to your variables.
-i2 = int(input())
-d2 = float(input())
-s2 = str(input())
-# Print the sum of both integer variables on a new line.
-print(i + i2)
-# Print the sum of the double variables on a new line.
-print(d + d2)
-# Concatenate and print the String variables on a new line
-# The 's' variable above should be printed first.
-print(s + s2)
+# Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
+s = input()
+
+# Print a string literal saying "Hello, World." to stdout.
+print('Hello, World.')
+
+# Write a line of code here that prints the contents of input_string to stdout.
+print(s)
