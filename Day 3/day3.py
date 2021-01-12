88c426b0829fb7eafbf7f26cb1878b480bd43544
Shahrullo <shahrullo1995@mail.ru>
2021-01-13 07:26:45 +0900

    Day 3. I did some stupid mistake and rewrite the day 4 to day 3 and now changed to its original

--- Day 3/day3.py
+++ Day 3/day3.py
@@ -1,29 +1,11 @@
-class Person:
-    def __init__(self,initialAge):
-        # Add some more code to run some checks on initialAge
-        if initialAge > 0:
-            self.age = initialAge
-        else:
-            print('Age is not valid, setting age to 0.')
-            self.age = 0
-    def amIOld(self):
-        # Do some computations in here and print out the correct statement to the console
-        if self.age < 13:
-            print('You are young.')
-        elif self.age >= 18:
-            print('You are old.')
-        else:
-            print('You are a teenager.')    
-    def yearPasses(self):
-        # Increment the age of the person in here
-        self.age += 1
+n = int(input())
 
-t = int(input())
-for i in range(0, t):
-    age = int(input())         
-    p = Person(age)  
-    p.amIOld()
-    for j in range(0, 3):
-        p.yearPasses()       
-    p.amIOld()
-    print("")
+if n % 2 != 0:
+    print('Weird')
+elif n % 2 == 0:
+    if n in range(2, 6):
+        print('Not Weird')
+    elif n in range(6, 21):
+        print('Weird')
+    elif n > 20:
+        print('Not Weird')
