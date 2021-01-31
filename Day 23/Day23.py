0eebef1da0dc07fb87480eb72ceaddc02595e6c3
Shahrullo <shahrullo1995@mail.ru>
2021-01-31 09:56:35 +0900

    Day 23. First commit

---
+++ Day 23/Day23.py
@@ -0,0 +1,39 @@
+class Node:
+    def __init__(self,data):
+        self.right=self.left=None
+        self.data = data
+class Solution:
+    def insert(self,root,data):
+        if root==None:
+            return Node(data)
+        else:
+            if data<=root.data:
+                cur=self.insert(root.left,data)
+                root.left=cur
+            else:
+                cur=self.insert(root.right,data)
+                root.right=cur
+        return root
+
+    def levelOrder(self,root):
+        #Write your code here
+        r = [root]
+        
+        while len(r) > 0:
+            node = r.pop(0)
+            print(node.data, end=' ')
+            if node.left:
+                r.append(node.left)
+            if node.right:
+                r.append(node.right)
+                
+        print()
+
+
+T=int(input())
+myTree=Solution()
+root=None
+for i in range(T):
+    data=int(input())
+    root=myTree.insert(root,data)
+myTree.levelOrder(root)
